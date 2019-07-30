85b3402a464935c47266d390b654bf9cb74781f1
spencercarr <46213261+spencercarr@users.noreply.github.com>
2019-07-30 01:17:58 -0400

    Refactored code, simplified.

--- src/main/java/com/rpg2014/invokers/EC2Invoker.java
+++ src/main/java/com/rpg2014/invokers/EC2Invoker.java
@@ -1,99 +1,31 @@
 package com.rpg2014.invokers;
 
+import com.rpg2014.wrappers.SpidermanEC2Wrapper;
 import software.amazon.awssdk.services.ec2.model.RunInstancesResponse;
 
+import javax.validation.constraints.NotNull;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class EC2Invoker {
 
-    /**
-     * Overloaded contructors that take an EC2Wrapper instance obj, a method name and arguments
-     */
-    public void EC2Invoker (Object obj, String methodName) throws InterruptedException {
-        Thread t = new Thread(setRunner(obj, methodName));
-        t.start();
-        t.join();
-    }
-
-    public void EC2Invoker (Object obj, String methodName, RunInstancesResponse runInstancesResponse) throws InterruptedException {
-        Thread t = new Thread(setRunner(obj, methodName, runInstancesResponse));
-        t.start();
-        t.join();
-    }
-
-    public void EC2Invoker (Object obj, String methodName, String instanceId) throws InterruptedException {
-        Thread t = new Thread(setRunner(obj, methodName, instanceId));
-        t.start();
-        t.join();
-    }
-
-    public void EC2Invoker (Object obj, String methodName, String oldAMIid, final String oldSnapshotId) throws InterruptedException {
-        Thread t = new Thread(setRunner(obj, methodName, oldAMIid, oldSnapshotId));
-        t.start();
-        t.join();
-    }
-
-    /**
-     * Overloaded Setters for the threads runnable
-     */
-    private Runnable setRunner (Object obj, String methodName) {
-        return () -> {
+    public void EC2Invoker (SpidermanEC2Wrapper instance, String methodName) throws InterruptedException {
+        Runnable r = () -> {
             Method method = null;
             try {
-                method = obj.getClass().getDeclaredMethod(methodName);
-            } catch (NoSuchMethodException e) { e.printStackTrace(); }
+                method = instance.getClass().getDeclaredMethod(methodName);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
 
-            assert method != null;
-            method.setAccessible(true);
             try {
-                method.invoke(obj);
-            } catch (IllegalAccessException | InvocationTargetException e) { e.printStackTrace(); }
+                method.invoke(instance);
+            } catch (IllegalAccessException | InvocationTargetException | AssertionError e) {
+                e.printStackTrace();
+            }
         };
-    }
-
-    private Runnable setRunner (Object obj, String methodName, RunInstancesResponse runInstancesResponse) {
-        return () -> {
-            Method method = null;
-            try {
-                method = obj.getClass().getDeclaredMethod(methodName);
-            } catch (NoSuchMethodException e) { e.printStackTrace(); }
-
-            assert method != null;
-            method.setAccessible(true);
-            try {
-                method.invoke(obj, runInstancesResponse);
-            } catch (IllegalAccessException | InvocationTargetException e) { e.printStackTrace(); }
-        };
-    }
-
-    private Runnable setRunner (Object obj, String methodName, String instanceId) {
-        return () -> {
-            Method method = null;
-            try {
-                method = obj.getClass().getDeclaredMethod(methodName);
-            } catch (NoSuchMethodException e) { e.printStackTrace(); }
-
-            assert method != null;
-            method.setAccessible(true);
-            try {
-                method.invoke(obj, instanceId);
-            } catch (IllegalAccessException | InvocationTargetException e) { e.printStackTrace(); }
-        };
-    }
-
-    private Runnable setRunner (Object obj, String methodName, String oldAMIid, final String oldSnapshotId) {
-        return () -> {
-            Method method = null;
-            try {
-                method = obj.getClass().getDeclaredMethod(methodName);
-            } catch (NoSuchMethodException e) { e.printStackTrace(); }
-
-            assert method != null;
-            method.setAccessible(true);
-            try {
-                method.invoke(obj, oldAMIid, oldSnapshotId);
-            } catch (IllegalAccessException | InvocationTargetException e) { e.printStackTrace(); }
-        };
+        Thread t = new Thread(r);
+        t.start();
+        t.join();
     }
 }

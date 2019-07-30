6f8ef942e7614c84013a6217e72c23755cda1619
rpg2014 <pgiven14@gmail.com>
2019-07-29 23:05:58 -0700

    Wrote of method for Status

--- src/main/java/com/rpg2014/model/Status.java
+++ src/main/java/com/rpg2014/model/Status.java
@@ -1,20 +1,25 @@
 package com.rpg2014.model;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public enum Status {
-    Pending ("pending"),
-    Running ("running"),
-    ShuttingDown("shutting-down"),
-    Terminated("terminated"),
-    Stopping("stopping"),
-    Stopped("stopped");
+    Pending(0),
+    Running(16),
+    ShuttingDown(32),
+    Terminated(48),
+    Stopping(64),
+    Stopped(80);
 
-    @Getter
-    String status;
-    Status(String status){
-        this.status = status;
+
+    int statusCode;
+
+    Status(int code ){
+        this.statusCode = code;
+    }
+
+    public Status of(int code ){
+        for (Status s : Status.values()){
+            if(s.statusCode == code)
+                return s;
+        }
+        return null;
     }
 }

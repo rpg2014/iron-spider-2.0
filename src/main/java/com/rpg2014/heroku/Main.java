859aec62e68aa0bced06773acab003e77c5fc161
rpg2014 <pgiven14@gmail.com>
2019-08-25 19:04:38 -0700

    Set log level

--- src/main/java/com/rpg2014/heroku/Main.java
+++ src/main/java/com/rpg2014/heroku/Main.java
@@ -1,5 +1,6 @@
 package com.rpg2014.heroku;
 
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -8,11 +9,13 @@ import org.glassfish.jersey.server.ResourceConfig;
  * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
  * command that is used for launching should fire this main method.
  */
+@Slf4j
 public class Main {
 
     public static void main(String[] args) throws Exception{
         // The port that we should run on can be set into an environment variable
         // Look for that variable and default to 8080 if it isn't there.
+
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
             webPort = "8080";

78b83711d3ae50b3b23f1ad35bb82ed12936dee8
rpg2014 <rpg2014@users.noreply.github.com>
2020-07-12 17:32:19 -0700

    change getProperty to getenv

--- src/main/java/com/rpg2014/ServerController.java
+++ src/main/java/com/rpg2014/ServerController.java
@@ -66,7 +66,7 @@ public class ServerController implements ServerControllerInterface {
     @Path("/details")
     public DetailsResponse serverDetails() {
         String domainName = (String) taskRunner.runEC2Task(Ec2MethodNames.DomainName).get();
-        String url = System.getProperty("minecraftURL");
+        String url = System.getenv("minecraftURL");
         log.info("url= " + url);
         return DetailsResponse.builder().domainName(url).build();
     }
